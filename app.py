9ff0574a83efbed9426630e04ad92635ebc39927
Deserlo <evamarienajera@csus.edu>
2021-09-11 22:38:01 -0700

    changed port since 5000 was in use

--- app.py
+++ app.py
@@ -168,5 +168,5 @@ def all_locals():
     return render_template("videos.html", title="Sacramento's Local Music Artists", videos=all_videos)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host='0.0.0.0', port=port)
